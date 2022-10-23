6f45f1e8a714e1f57301386b697c8f84a3e64736
RedaMeziouni <meziounidb@gmail.com>
2022-10-23 11:52:10 +0100

    grab the necessary elements

--- CSV Reader/src/app.ts
+++ CSV Reader/src/app.ts
@@ -1,1 +1,3 @@
-console.log("Hellow, World!");
+const csvForm = document.getElementById('csvForm') as HTMLFormElement;
+const vscFile = document.getElementById('csvFile') as HTMLInputElement;
+const displayArea = document.getElementById('displayArea') as HTMLDivElement;
