59b3021512424c035da857cd453e9ed1f1a0b13a
RedaMeziouni <meziounidb@gmail.com>
2022-10-23 12:15:28 +0100

    Create generate table function that map over the table array

--- CSV Reader/src/app.ts
+++ CSV Reader/src/app.ts
@@ -1,3 +1,44 @@
 const csvForm = document.getElementById('csvForm') as HTMLFormElement;
-const vscFile = document.getElementById('csvFile') as HTMLInputElement;
+const csvFile = document.getElementById('csvFile') as HTMLInputElement;
 const displayArea = document.getElementById('displayArea') as HTMLDivElement;
+
+let final_vals = [];
+
+csvForm.addEventListener('submit', (e: Event) => {
+    // prevent default submit behavior
+    e.preventDefault();
+
+    let csvReader = new FileReader();
+
+    const input = csvFile.files[0];
+
+    csvReader.readAsText(input);
+});
+
+const generate_table = async (arrayTable: [string[]]): Promise<string> => {
+    return `
+        <table class="table table-striped">
+            <thead>
+                ${arrayTable[0].map(val => {
+                    return `
+                        <th scope="col">${val}</th>
+                    `
+                }).join('') }
+            </thead>
+            <tbody>
+                ${arrayTable.map((val, index) => {
+                    if(index === 0) return;
+                    return `
+                        <tr>
+                            ${val.map(sub_val => {
+                                return `
+                                    <td>${sub_val}</td>
+                                `
+                            }).join('')}
+                        </tr>
+                    `
+                }).join('')}
+            </tbody>
+        </table>
+    `;
+}
