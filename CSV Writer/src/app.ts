76f04768c417a26d66dfa5c882783b257b7d355b
RedaMeziouni <meziounidb@gmail.com>
2022-10-28 13:22:17 +0100

    bring the columns creating on the onloading

--- CSV Writer/src/app.ts
+++ CSV Writer/src/app.ts
@@ -1,1 +1,8 @@
-console.log('Hello, World!');
+import { createNewColArea } from "./createColumns";
+
+const centerScreen = document.getElementById('centerScreen') as HTMLDivElement;
+const columnList = [] as string[];
+
+window.onload = () => {
+    centerScreen.append(createNewColArea(columnList, function() {}));
+}
