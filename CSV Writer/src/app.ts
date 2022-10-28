daa328ae40b703627677ceaf08ea704ffa3a640e
RedaMeziouni <meziounidb@gmail.com>
2022-10-28 13:45:27 +0100

    create changeState function that doesn't return anything and call it on the onloading

--- CSV Writer/src/app.ts
+++ CSV Writer/src/app.ts
@@ -1,8 +1,13 @@
 import { createNewColArea } from "./createColumns";
+import { createTable } from './createCsv';
 
 const centerScreen = document.getElementById('centerScreen') as HTMLDivElement;
 const columnList = [] as string[];
+const changeState = (): void => {
+    centerScreen.innerHTML = '';
+    centerScreen.append(createTable(columnList));
+}
 
 window.onload = () => {
-    centerScreen.append(createNewColArea(columnList, function() {}));
+    centerScreen.append(createNewColArea(columnList, changeState));
 }
